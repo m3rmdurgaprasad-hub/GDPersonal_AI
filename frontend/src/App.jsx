8678ae0cc655d8888ed91a159ee1ebe4575889b4
m3rmdurgaprasad-hub <m3.rmdurgaprasad@gmail.com>
2026-02-28 21:03:18 +0530

    Initialize App component with ChatBox

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import ChatBox from "./components/ChatBox";
+
+function App() {
+  return (
+    <div>
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
